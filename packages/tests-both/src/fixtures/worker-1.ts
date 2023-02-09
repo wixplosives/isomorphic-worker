5e95e8f2a187c0cbb10d3f008eeb7fdd7728c950
Yarden Porat <yardenporat@gmail.com>
2023-02-09 17:46:14 +0200

    test: adjust nested worker test

--- packages/tests-both/src/fixtures/worker-1.ts
+++ packages/tests-both/src/fixtures/worker-1.ts
@@ -1,12 +1,18 @@
-import { worker } from '@wixc3/isomorphic-worker/worker-scope';
 import { Worker } from '@wixc3/isomorphic-worker/worker';
+import { worker } from '@wixc3/isomorphic-worker/worker-scope';
 
 // eslint-disable-next-line no-console
-worker.addEventListener('error', (e) => console.log(e));
-worker.addEventListener('message', (e) => {
-    const worker2 = new Worker('./worker-2.js'); // see webpack.config.js
-    worker2.addEventListener('message', (message: any) => {
-        worker.postMessage(message.data);
+const log = console.log.bind(console, 'worker1 | ');
+
+worker.addEventListener('error', (e) => log(e));
+worker.addEventListener('message', (_e) => {
+    // eslint-disable-next-line no-console
+    log('first worker listener attached');
+
+    const worker2 = new Worker('./worker-2.js');
+    worker2.addEventListener('message', ({ data }: any) => {
+        log('data', data);
+        worker.postMessage(data);
     });
-    worker2.postMessage(e.data);
+    worker2.postMessage('Hello from worker 1');
 });
--- packages/tests-both/src/fixtures/worker-2.ts
+++ packages/tests-both/src/fixtures/worker-2.ts
@@ -1,10 +1,12 @@
 import { worker } from '@wixc3/isomorphic-worker/worker-scope';
 
 // eslint-disable-next-line no-console
-worker.addEventListener('error', (e) => console.log(e));
-worker.addEventListener('message', (e) => {
-    // eslint-disable-next-line no-console
-    console.log(`Second worker received: ${e.data}`);
+const log = console.log.bind(console, 'worker2 | ');
 
+// eslint-disable-next-line no-console
+worker.addEventListener('error', (e) => log(e));
+worker.addEventListener('message', ({ data }: { data: unknown }) => {
+    log(data);
     worker.postMessage('Hello from worker 2 (nested worker)');
 });
+log('second worker listener attached');
