40a33cc0194812d1e9c13b90e43b939ad512b3b4
Avi Vahl <avi.vahl@wix.com>
2024-12-18 16:53:18 +0200

    feat!: mark package as having no side effects (#20)
    remove no-longer-needed polyfill, as Safari implemented support for Worker in Worker around March 2023 (IIRC when adding support for ServiceWorker). verified it works on Noam's machine.

--- packages/isomorphic-worker/src/browser-polyfill/is-safari.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This a polyfill for safari, which does not support the `new Worker` constructor in a worker scope.
- * We use this flag to detect if we are in a safari.
- *
- * We chose using this flag over feature detection, because we have to extend the `Worker` class,
- * which we prefer not to do if we don't have to.
- */
-export const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
